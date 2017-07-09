21b2b0011993522e65c56094cf57edf7fa3a865b
ownCloud Jenkins <jenkins@owncloud.com>
2017-07-09 01:51:19 +0000

    [tx-robot] updated from transifex

--- l10n/bg_BG.js
+++ l10n/bg_BG.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Step 1 failed. Exception: %s" : "Стъпка 1 - неуспешна. Грешка: %s",
     "Step 2 failed. Exception: %s" : "Стъпка 2 - неуспешна. Грешка: %s",
     "Google Drive App Configuration" : "Настройки на Google Drive приложение",
+    "Error verifying OAuth2 Code for " : "Грешка при проверката на на OAuth2 кодър за",
     "Google Drive" : "Google Drive"
 },
 "nplurals=2; plural=(n != 1);");
