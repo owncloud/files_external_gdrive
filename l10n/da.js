f31581765700e63083b31d9e0ca9393c3fc45639
ownCloud Jenkins <jenkins@owncloud.com>
2017-07-29 01:51:17 +0000

    [tx-robot] updated from transifex

--- l10n/da.js
+++ l10n/da.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Step 1 failed. Exception: %s" : "Trin 1 mislykkedes. Undtagelse: %s",
     "Step 2 failed. Exception: %s" : "Trin 2 mislykkedes. Undtagelse: %s",
     "Google Drive App Configuration" : "Google Drive App konfiguration",
+    "Error verifying OAuth2 Code for " : "Fejl ved verifikation af OAuth2 code for",
     "Google Drive" : "Google Drev"
 },
 "nplurals=2; plural=(n != 1);");
