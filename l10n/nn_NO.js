55b1ebdb7ce44487d5132114f117da5b2cd57318
ownCloud Jenkins <jenkins@owncloud.com>
2017-11-20 01:53:07 +0000

    [tx-robot] updated from transifex

---
+++ l10n/nn_NO.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_external_gdrive",
+    {
+    "Google Drive App Configuration" : "Google Drive program konfigurasjon",
+    "Error verifying OAuth2 Code for " : "Feil ved verifisering av OAuth2 kode for",
+    "Google Drive" : "Google Drive",
+    "Step 1 failed. Exception: %s" : "Steg 1 feila. Feil: %s",
+    "Step 2 failed. Exception: %s" : "Steg 2 feila. Feil: %s"
+},
+"nplurals=2; plural=(n != 1);");
