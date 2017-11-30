8c6b32bebbd1de12c4ee23cf676e240e027fd381
ownCloud Jenkins <jenkins@owncloud.com>
2017-11-30 02:05:37 +0000

    [tx-robot] updated from transifex

--- l10n/sq.js
+++ l10n/sq.js
@@ -2,6 +2,7 @@ OC.L10N.register(
     "files_external_gdrive",
     {
     "Google Drive App Configuration" : "Formësim i Aplikacionit Google Drive",
+    "Error verifying OAuth2 Code for " : "Gabim në verifikimin e Kodit OAuth2 për",
     "Google Drive" : "Google Drive",
     "Step 1 failed. Exception: %s" : "Hapi 1 dështoi. Përjashtim: %s",
     "Step 2 failed. Exception: %s" : "Hapi 2 dështoi. Përjashtim: %s"
