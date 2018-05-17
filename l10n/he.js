7f962bf1baebf07d8b32f735c061a90c4643677f
ownCloud Jenkins <jenkins@owncloud.com>
2018-05-17 01:51:22 +0000

    [tx-robot] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -7,4 +7,4 @@ OC.L10N.register(
     "Step 1 failed. Exception: %s" : "שלב 1 נכשל. חריג: %s",
     "Step 2 failed. Exception: %s" : "שלב 2 נכשל. חריג: %s"
 },
-"nplurals=2; plural=(n != 1);");
+"nplurals=4; plural=(n==1 ? 1: n == 2 ? 2 : (n % 10 == 0 and n > 10) ? 4);");
