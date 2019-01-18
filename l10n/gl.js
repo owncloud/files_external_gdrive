5a9cbc463e05b980334a5bd71622d819567bb58d
ownClouders <devops@owncloud.com>
2019-01-18 05:34:04 +0000

    [tx] updated from transifex

---
+++ l10n/gl.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_external_gdrive",
+    {
+    "Google Drive App Configuration" : "Configuración do aplicativo do Gogle Drive",
+    "Error verifying OAuth2 Code for " : "Produciuse un erro a verificar o código OAuth2 para",
+    "Google Drive" : "Google Drive",
+    "Step 1 failed. Exception: %s" : "Fallou o paso 1. Excepción: %s",
+    "Step 2 failed. Exception: %s" : "Fallou o paso 2. Excepción: %s"
+},
+"nplurals=2; plural=(n != 1);");
