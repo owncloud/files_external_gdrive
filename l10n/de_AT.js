2cbbd8d7f4ab52903f62dfb46f4ed263b411a759
ownClouders <devops@owncloud.com>
2019-07-11 05:01:46 +0000

    [tx] updated from transifex

---
+++ l10n/de_AT.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_external_gdrive",
+    {
+    "Google Drive App Configuration" : "Google Drive App Konfigurierung",
+    "Error verifying OAuth2 Code for " : "Bei der Verifikation des OAuth2 Codes für [ ] ist ein Fehler aufgetreten.",
+    "Google Drive" : "Google Drive",
+    "Step 1 failed. Exception: %s" : "Schritt 1 ist fehlgeschlagen. Ausnahme: %s",
+    "Step 2 failed. Exception: %s" : "Schritt 2 ist fehlgeschlagen. Ausnahme: %s"
+},
+"nplurals=2; plural=(n != 1);");
