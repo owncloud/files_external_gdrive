e6306079710c26edeedb5221ababb096e946ef1d
ownClouders <devops@owncloud.com>
2019-09-03 01:12:57 +0000

    [tx] updated from transifex

---
+++ l10n/eu.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_external_gdrive",
+    {
+    "Google Drive App Configuration" : "Google Drive App Ezarpenak",
+    "Error verifying OAuth2 Code for " : "Errorea gertatu da OAuth2 Kodea egiaztatzean honentzat",
+    "Google Drive" : "Google Drive",
+    "Step 1 failed. Exception: %s" : "1 Urratsak huts egin du. Salbuespena: %s",
+    "Step 2 failed. Exception: %s" : "2 Urratsak huts egin du. Salbuespena: %s"
+},
+"nplurals=2; plural=(n != 1);");
