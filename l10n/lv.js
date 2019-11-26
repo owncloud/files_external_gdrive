87fb652e659e2cec6fb429dbe1e1fa32fb7c0883
Dominik Schmidt <domme@tomahawk-player.org>
2019-11-26 15:04:11 +0000

    [tx] updated from transifex

---
+++ l10n/lv.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_external_gdrive",
+    {
+    "Google Drive App Configuration" : "Google Drive Lietotnes Konfigurācija",
+    "Error verifying OAuth2 Code for " : "Kļūda verificējot OAuth2 kodu priekš ",
+    "Google Drive" : "Google Drive",
+    "Step 1 failed. Exception: %s" : "Pirmais solis neizdevās. Izņēmums: %s",
+    "Step 2 failed. Exception: %s" : "Otrais solis neizdevās. Izņēmums: %s"
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);");
