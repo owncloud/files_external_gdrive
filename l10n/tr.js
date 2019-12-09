3df77323916fe3bab57da579d2f5aa9ae38824e0
Dominik Schmidt <domme@tomahawk-player.org>
2019-12-09 16:42:42 +0000

    [tx] updated from transifex

--- l10n/tr.js
+++ l10n/tr.js
@@ -3,7 +3,7 @@ OC.L10N.register(
     {
     "Google Drive App Configuration" : "Google Drive Uygulama Yapılandırması",
     "Google Drive" : "Google Drive",
-    "Step 1 failed. Exception: %s" : "Adım 1 başarısız. Özel durum: %s",
-    "Step 2 failed. Exception: %s" : "Adım 2 başarısız. Özel durum: %s"
+    "Step 1 failed. Exception: %s" : "Adım 1 başarısız. İstisna: %s",
+    "Step 2 failed. Exception: %s" : "Adım 2 başarısız. İstisna: %s"
 },
 "nplurals=2; plural=(n > 1);");
