2e0b1622aa280bd1f8ab84a9385d2784a13a32fa
Dominik Schmidt <domme@tomahawk-player.org>
2020-01-16 02:25:53 +0000

    [tx] updated from transifex

---
+++ l10n/ar.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_external_gdrive",
+    {
+    "Google Drive App Configuration" : "تكوين تطبيق Google Drive",
+    "Error verifying OAuth2 Code for " : "خطأ في التحقق من صحة رمز OAuth2 لـ",
+    "Google Drive" : "Google Drive",
+    "Step 1 failed. Exception: %s" : "فشلت الخطوة 1. استثناء: %s",
+    "Step 2 failed. Exception: %s" : "فشلت الخطوة 2. استثناء: %s"
+},
+"nplurals=6; plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 && n%100<=99 ? 4 : 5;");
