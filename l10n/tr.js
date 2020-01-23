73d607be53f1774b705df21206c897081a4086cf
Dominik Schmidt <domme@tomahawk-player.org>
2020-01-23 02:27:17 +0000

    [tx] updated from transifex

--- l10n/tr.js
+++ l10n/tr.js
@@ -2,6 +2,7 @@ OC.L10N.register(
     "files_external_gdrive",
     {
     "Google Drive App Configuration" : "Google Drive Uygulama Yapılandırması",
+    "Error verifying OAuth2 Code for " : "OAuth2 Kodu doğrulanırken hata oluştu",
     "Google Drive" : "Google Drive",
     "Step 1 failed. Exception: %s" : "Adım 1 başarısız. İstisna: %s",
     "Step 2 failed. Exception: %s" : "Adım 2 başarısız. İstisna: %s"
