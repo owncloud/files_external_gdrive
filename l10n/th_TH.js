fc7dabbe3bfdaa6bd35de657977abe8d14b20735
Thomas Müller <thomas.mueller@tmit.eu>
2020-11-29 00:09:13 +0000

    [tx] updated from transifex

--- l10n/th_TH.js
+++ l10n/th_TH.js
@@ -3,7 +3,7 @@ OC.L10N.register(
     {
     "Google Drive App Configuration" : "ตั้งค่าแอปฯ Google Drive",
     "Error verifying OAuth2 Code for " : "เกิดข้อผิดพลาดในการยืนยันรหัส OAuth2 สำหรับ",
-    "Google Drive" : "กูเกิ้ลไดร์ฟ",
+    "Google Drive" : "Google Drive",
     "Step 1 failed. Exception: %s" : "ขั้นตอนที่ 1 ล้มเหลว ข้อยกเว้น: %s",
     "Step 2 failed. Exception: %s" : "ขั้นตอนที่ 2 ล้มเหลว ข้อยกเว้น: %s"
 },
