1483df49f2e205c53bd132326bf4998af7f5478d
ownClouders <devops@owncloud.com>
2022-10-05 00:44:13 +0000

    [tx] updated from transifex

--- l10n/bg_BG.js
+++ l10n/bg_BG.js
@@ -2,9 +2,9 @@ OC.L10N.register(
     "files_external_gdrive",
     {
     "Google Drive App Configuration" : "Настройки на Google Drive приложение",
-    "Error verifying OAuth2 Code for " : "Грешка при проверката на на OAuth2 кодър за",
+    "Error verifying OAuth2 Code for " : "Грешка при проверката на кода OAuth2 за ",
     "Google Drive" : "Google Drive",
-    "Step 1 failed. Exception: %s" : "Стъпка 1 - неуспешна. Грешка: %s",
-    "Step 2 failed. Exception: %s" : "Стъпка 2 - неуспешна. Грешка: %s"
+    "Step 1 failed. Exception: %s" : "Стъпка 1 е неуспешна. Изключение: %s",
+    "Step 2 failed. Exception: %s" : "Стъпка 2 е неуспешна. Изключение: %s"
 },
 "nplurals=2; plural=(n != 1);");
