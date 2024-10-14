e5eb2952e832b49a2eb074e588c79e1ddeb054fd
ownClouders <devops@owncloud.com>
2024-10-14 01:38:04 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_external_gdrive",
+    {
+    "Google Drive App Configuration" : "Google Drive ئەپ سەپلىمىسى",
+    "Error verifying OAuth2 Code for " : "ئۈچۈن OAuth2 كودىنى دەلىللەشتە خاتالىق",
+    "Google Drive" : "Google Drive",
+    "Step 1 failed. Exception: %s" : "1-قەدەم مەغلۇپ بولدى. مۇستەسنا:% s",
+    "Step 2 failed. Exception: %s" : "ئىككىنچى قەدەم مەغلۇپ بولدى. مۇستەسنا:% s"
+},
+"nplurals=2; plural=(n != 1);");
